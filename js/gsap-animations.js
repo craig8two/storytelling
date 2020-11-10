552bae9bee6f4f94614ad9be68f4c250b40831a0
ZEHRA RIZVI <zehrataqi@gmail.com>
2020-11-10 19:35:16 +0300

    map section work

--- js/gsap-animations.js
+++ js/gsap-animations.js
@@ -61,18 +61,30 @@ var lifecycle_approach_scene = new ScrollMagic.Scene({
 
 //MAP SECTION
 
-var maps_timeline = gsap.timeline({});
+var maps_timeline = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#maps-section",
+		pin: "body",
+		start: "center center",
+		end: "1000%",
+		markers: "true",
+		scrub: 1
+	}
+});
 
-maps_timeline.to("#map-1", { opacity: 0, display: "none", duration: 0.2 }, "+=5");
-maps_timeline.to("#map-2", { opacity: 1, display: "block", duration: 0.2 });
-maps_timeline.to("#map-2", { opacity: 0, display: "none", duration: 0.2 }, "+=5");
-maps_timeline.to("#map-3", { opacity: 1, display: "block", duration: 0.2 });
+maps_timeline.from("#map-1-text", { x: -100, opacity: 0, delay: 3, duration: 1000, ease: "power4.out" });
+maps_timeline.to("#map-1-text", { x: -100, opacity: 0, delay: 5, duration: 1000, ease: "power4.out" });
+maps_timeline.to(".mini-map", { y: -100, opacity: 0, delay: 5, duration: 1000, ease: "power4.out" });
+maps_timeline.to("#map-1", { opacity: 0, duration: 0, ease: "power4.out" });
 
-var maps_scene = new ScrollMagic.Scene({
-	triggerElement: "#maps-section",
-	duration: "200%",
-	triggerHook: 0
-})
-	.setPin("#maps-section")
-	.setTween(maps_timeline) // trigger a TweenMax.to tween
-	.addTo(controller);
+maps_timeline.to("#map-2", { opacity: 1, duration: 1000, ease: "power4.out" }, "+=50");
+maps_timeline.from("#map-2-text-1", { x: -100, opacity: 0, delay: 10, duration: 1000, ease: "power4.out" }, "+=500");
+maps_timeline.from("#map-2-text-2", { x: -100, opacity: 0, delay: 3, duration: 1000, ease: "power4.out" }, "+=100");
+
+maps_timeline.to("#map-2-text-1", { x: -100, opacity: 0, delay: 10, duration: 1000, ease: "power4.out" });
+maps_timeline.to("#map-2-text-2", { x: -100, opacity: 0, delay: 3, duration: 1000, ease: "power4.out" });
+maps_timeline.to("#map-2", { opacity: 0, delay: 3, duration: 0.1, ease: "power4.out" });
+
+maps_timeline.to("#map-3", { opacity: 1, duration: 2, ease: "power4.out" }, "+=50");
+maps_timeline.from("#map-3-text-1", { x: -100, opacity: 0, delay: 10, duration: 1000, ease: "power4.out" }, "+=500");
+maps_timeline.from("#map-3-text-2", { x: -100, opacity: 0, delay: 3, duration: 1000, ease: "power4.out" }, "+=100");
